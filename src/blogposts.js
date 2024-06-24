bcea2eb17df54737552a65d8eef0710f4a1efa96
Tobias Konerding <tobias.konerding@gmail.com>
2024-06-24 14:19:04 +0200

    Install Tailwind and finalize setup

---
+++ src/blogposts.js
@@ -0,0 +1,26 @@
+/* Array mit allen Blog-Artikeln */
+let blogposts = [
+  {
+    arrayNo: 0,
+    autor: "Tobias",
+    headline: "Das ist der 1. Artikel",
+    posttext:
+      "Habanero golden apricot smoky maple tempeh glaze raspberry fizz creamy cauliflower alfredo seeds udon noodles blackberries refreshing cucumber splash banana bread cocoa kung pao pepper summer fruit salad strawberries spring chocolate ultra creamy avocado pesto soup macadamia nut cookies tofu basil. Sriracha pecans matcha vine tomatoes walnut pesto tart lemon tahini dressing mint lime taco salsa pineapple salsa candy cane winter ginger tofu pomegranate oranges portobello mushrooms cherry bomb pepper cinnamon toast picnic. Instant pot fruit smash Thai dragon pepper double dark chocolate coriander chocolate peanut butter dip naga viper miso turmeric glazed aubergine Bolivian rainbow pepper guacamole lychee roasted peanuts Thai basil curry spicy Thai super chili.",
+  },
+  {
+    arrayNo: 1,
+    autor: "Hans Wurst",
+    headline: "Das ist der 2. Artikel",
+    posttext:
+      "asdmnsjdad winter ginger tofu pomegranate oranges portobello mushrooms cherry bomb pepper cinnamon toast picnic. Instant pot fruit smash Thai dragon pepper double dark chocolate coriander chocolate peanut butter dip naga viper miso turmeric glazed aubergine Bolivian rainbow pepper guacamole lychee roasted peanuts Thai basil curry spicy Thai super chili.",
+  },
+  {
+    arrayNo: 2,
+    autor: "Thomas Gottschalk",
+    headline: "Das ist der 3. Artikel",
+    posttext:
+      "Leek muffins shiitake mushrooms figs eating together Chinese five-spice powder lentils artichoke hearts appetizer grapefruit Italian linguine puttanesca green grapes jalapeño chai tea green tea Italian pepperoncini entree homemade balsamic. Crunchy seaweed tasty crispy green tea lime soy milk sweet potato rich coconut cream roasted butternut squash kale ginger lemongrass agave green tea tomato and basil cashew shallots parsley overflowing sandwiches.",
+  },
+];
+
+export default blogposts;
