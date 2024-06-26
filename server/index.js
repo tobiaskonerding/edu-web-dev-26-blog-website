ba16a2756091975abc3009fd42a2eb8f9ef0f8e6
Tobias Konerding <tobias.konerding@gmail.com>
2024-06-26 10:30:33 +0200

    Setup node-express backend

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
